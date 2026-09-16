fc2cd2a43e58def8747221021423f40929c2bf0d
1Minak <minak_st01@mail.ru>
2026-09-16 12:53:09 +0300

    Сделал 8 задание

--- 01_python_basics/practice/01_intro_tasks/task8_bench_timer.py
+++ 01_python_basics/practice/01_intro_tasks/task8_bench_timer.py
@@ -11,3 +11,8 @@
 Введите время в секундах: 3661
 1:01:01
 """
+sec = int(input('Введите время в секундах: '))
+hours = sec // 3600
+minutes = (sec - hours * 3600) // 60
+seconds = sec - (hours * 3600 + minutes * 60)
+print(f'{hours} : {minutes:02d} : {seconds:02d}')
