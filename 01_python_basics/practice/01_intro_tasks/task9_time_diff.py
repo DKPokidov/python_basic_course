d9884ca85eab3dbbaaf513163913018588647dec
1Minak <minak_st01@mail.ru>
2026-09-16 13:10:12 +0300

    Сделал 9 задание

--- 01_python_basics/practice/01_intro_tasks/task9_time_diff.py
+++ 01_python_basics/practice/01_intro_tasks/task9_time_diff.py
@@ -17,3 +17,16 @@
 Секунда ухода: 0
 3:30:00
 """
+hour1 = int(input('Час прихода: '))
+minute1 = int(input('Минута прихода: '))
+second1 = int(input('Секунда прихода:'))
+hour2 = int(input('Час ухода: '))
+minute2 =  int(input('Минута ухода: '))
+second2 = int(input('Секунда ухода: '))
+sec1 = hour1 * 3600 + minute1 * 60 + second1
+sec2 = hour2 * 3600 + minute2 * 60 + second2
+diff = sec2 - sec1
+hours = diff // 3600
+minutes = (diff - hours * 3600) // 60
+seconds = diff - (hours * 3600 + minutes * 60)
+print(f'{hours}: {minutes:02d}: {seconds:02d}')
