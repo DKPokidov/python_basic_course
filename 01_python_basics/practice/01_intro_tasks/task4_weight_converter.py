b7ae0809a9ebed70d63752f8cf8eafd9e07cbaad
Риана <mindalorex@gmail.com>
2026-09-16 20:55:22 +0300

    задания 3-4 модуль 1

--- 01_python_basics/practice/01_intro_tasks/task4_weight_converter.py
+++ 01_python_basics/practice/01_intro_tasks/task4_weight_converter.py
@@ -20,17 +20,17 @@
 1.0 т = 1000.000000 кг
 """
 
-weight, bukva = input('Введите вес и единицу измерения (например, "10 кг"): ').strip().split()
+weight, bukva = input("Введите вес и единицу измерения (например, '10 кг'): ").strip().split()
 
 if bukva == 'кг':
     print(f'{weight} кг = {weight} кг')
 elif bukva == 'г':
-    print(f'{float(weight)} г = {float(weight) * 10**-3} кг')
+    print(f'{float(weight)} г = {(float(weight) * 10**-3):.6f} кг')
 elif bukva == 'пуд':
-    print(f'{float(weight)} пуд = {float(weight) * 16} кг')
-elif bukva =='фунт':
-    print(f'{float(weight)} фунт = {float(weight) * 0.40951241} кг')
+    print(f'{float(weight)} пуд = {(float(weight) * 16):.6f} кг')
+elif bukva == 'фунт':
+    print(f'{float(weight)} фунт = {(float(weight) * 0.40951241):.6f} кг')
 elif bukva == 'ц':
-    print(f'{float(weight)} ц = {float(weight) * 100} кг')
+    print(f'{float(weight)} ц = {(float(weight) * 100):.6f} кг')
 elif bukva == 'т':
-    print(f'{float(weight)} т = {float(weight) * 1000} кг')
+    print(f'{float(weight)} т = {(float(weight) * 1000):.6f} кг')
