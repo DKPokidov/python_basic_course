278665968d70ce20ff306cf0c49e6788a711b8e8
aliyaa3 <ak4urina.aliya@yandex.ru>
2026-09-18 11:01:45 +0300

    Исправление: модуль 1

--- 01_python_basics/practice/01_intro_tasks/task3_coins.py
+++ 01_python_basics/practice/01_intro_tasks/task3_coins.py
@@ -21,7 +21,7 @@ fifty = int(input('Сколько монет по 50 копеек? '))
 ten = int(input('Сколько монет по 10 копеек? '))
 five = int(input('Сколько монет по 5 копеек? '))
 one = int(input('Сколько монет по 1 копейке? '))
-summa = fifty*50 + ten*10 + five*5 + one
+summa = fifty * 50 + ten * 10 + five * 5 + one
 rubles = summa // 100
 kop = summa % 100
 print(rubles, 'руб.', kop, 'коп.')
