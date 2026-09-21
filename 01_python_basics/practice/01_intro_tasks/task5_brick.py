00e6124336a2cb5485044a388bbb631fe2e3ff51
Ernest-Mansurov-ITMO <ernest.mansurov@icloud.com>
2026-09-21 13:59:25 +0300

    Исправление задач модуля 1

--- 01_python_basics/practice/01_intro_tasks/task5_brick.py
+++ 01_python_basics/practice/01_intro_tasks/task5_brick.py
@@ -34,7 +34,7 @@ area = 2 * (lenght * width + width * height + height * lenght)
 sum_of_dimensions = 4 * (lenght + width + height)
 mass = volume/(10**6) * 1700
 quantity = 10**6//volume
-d = [f"Объём: {volume} см³", f"Площадь поверхности: {area} см²", f"Сумма рёбер: {sum_of_dimensions} см", f"Масса: {mass} кг", \
+d = [f"Объём: {volume:.2f} см³", f"Площадь поверхности: {area:.2f}", f"Сумма рёбер: {sum_of_dimensions:.2f} см", f"Масса: {mass:.2f} кг", \
       f"Количество кирпичей в 1 м³: {quantity}"]
 for i in d:
     print(i)
